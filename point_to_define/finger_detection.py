177b971ab8a1812c420834845bf61a98d2942e92
Ben Meline <benjaminmeline2015@u.northwestern.edu>
2014-12-01 12:00:21 -0600

    Skin histogram and mask

--- point_to_define/finger_detection.py
+++ point_to_define/finger_detection.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import copy
 
 class FingerDetection:
 	def __init__(self):
@@ -17,27 +18,54 @@ class FingerDetection:
 	def draw_rectangle(self, frame):
 		"""draw rectangles on frame"""
 		rows,cols,_ = frame.shape
-		self.x_nw = [9*cols/20,10*cols/20,11*cols/20,
-						9*cols/20,10*cols/20,11*cols/20,
-						9*cols/20,10*cols/20,11*cols/20]
-		self.y_nw = [6*rows/20,6*rows/20,6*rows/20,
-						10*rows/20,10*rows/20,10*rows/20,
-						14*rows/20,14*rows/20,14*rows/20]
+		self.row_nw = np.array([6*rows/20,6*rows/20,6*rows/20,
+														10*rows/20,10*rows/20,10*rows/20,
+														14*rows/20,14*rows/20,14*rows/20])
+
+		self.col_nw = np.array([9*cols/20,10*cols/20,11*cols/20,
+														9*cols/20,10*cols/20,11*cols/20,
+														9*cols/20,10*cols/20,11*cols/20])
+
+		self.row_se = np.zeros(self.row_nw.size, dtype=np.int)
+		for i in range(self.row_nw.size):
+			self.row_se[i] = self.row_nw[i] + 10
+
+		self.col_se = np.zeros(self.col_nw.size, dtype=np.int)
+		for i in range(self.col_nw.size):
+			self.col_se[i] = self.col_nw[i] + 10	
+
+		for i in range(self.row_nw.size):
+			cv2.rectangle(frame,(self.col_nw[i],self.row_nw[i]),(self.col_se[i],self.row_se[i]),(0,255,0),1)
+
+	def set_skin_hist(self, frame):
+		#TODO use constants, only do HSV for ROI
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		roi = np.zeros([90,10,3], dtype=hsv.dtype)
 		
-		self.x_se = []
-		for x in self.x_nw:
-			self.x_se.append(x+10)
+		for i in range(self.row_nw.size):
+			roi[i*10:i*10+10,0:10] = hsv[self.row_nw[i]:self.row_nw[i]+10,
+																	 self.col_nw[i]:self.col_nw[i]+10]
 
-		self.y_se = []
-		for y in self.y_nw:
-			self.y_se.append(y+10)	
+		self.roihist = cv2.calcHist([roi],[0, 1], None, [180, 256], [0, 180, 0, 256])																		
+		cv2.normalize(self.roihist,self.roihist,0,255,cv2.NORM_MINMAX)																		
 
-		for i in range(len(self.x_nw)):
-			cv2.rectangle(frame,(self.x_nw[i],self.y_nw[i]),(self.x_se[i],self.y_se[i]),(0,255,0),1)
-	
-	def get_rectangle_pixels(self):
-		"""select pixels in rectangles"""
+	def skin_hist_mask(self, frame):
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		dst = cv2.calcBackProject([hsv],[0,1],self.roihist,[0,180,0,256],1)
+		
+		# disc2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+		# cv2.morphologyEx(dst,cv2.MORPH_CLOSE,disc2,dst,iterations=2)
 
+		disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11))
+		cv2.filter2D(dst,-1,disc,dst)
+			
+		ret,thresh = cv2.threshold(dst,175,255,0)
+		thresh = cv2.merge((thresh,thresh,thresh))
+		
+		cv2.GaussianBlur(dst,(3,3),0,dst)
+
+		res = cv2.bitwise_and(frame,thresh)
+		return res
 
 def main():
 	fd = FingerDetection()
@@ -47,17 +75,24 @@ def main():
 		(grabbed, frame) = camera.read()
 
 		frame = fd.resize(frame)
+		frame_orig = copy.deepcopy(frame)
+
 		if not fd.finished_training:
 			fd.draw_rectangle(frame)
 
-		if cv2.waitKey(1) == ord('p'):
-			# fd.get_rectangle_pixels()
-			fd.finished_training = True
+		if cv2.waitKey(1) == ord('t'):
+			fd.set_skin_hist(frame_orig)
+			fd.finished_training = True	
 
 		if cv2.waitKey(1) == ord('q'):
 			break
 	
-		cv2.imshow('image', frame)	
+		if fd.finished_training:
+			skin = fd.skin_hist_mask(frame)
+			cv2.imshow('image', skin)
+		else:
+			cv2.imshow('image', frame)
+
 
 	camera.release()
 	cv2.destroyAllWindows()				
